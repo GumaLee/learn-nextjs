b67dc2b8cfc17de038a6a7bf6a0c562704a90034
Minwoo Lee <rkdxhd97@naver.com>
2024-02-16 17:27:46 +0900

    Feat: finish project

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movie-videos.module.css"
 
 /**
  * id로 트레일러 영상 가져오기
@@ -6,13 +7,25 @@ import { API_URL } from "../app/(home)/page";
  * @returns videos json
  */
 async function getVideos(id: string) {
-    // throw new Error('[ERROR] something broke...')
-    const response = await fetch(`${API_URL}/${id}/videos`);
-    return response.json();
+  // throw new Error('[ERROR] something broke...')
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
 }
 
 // video에 관한 UI만을 가지는 컴포넌트
-export default async function MovieVideos({id}: {id:string}) {
-    const videos = await getVideos(id);
-    return <h6>{JSON.stringify(videos)}</h6>
+export default async function MovieVideos({ id }: { id: string }) {
+  const videos = await getVideos(id);
+  return (
+    <div className={styles.container}>
+      {videos.map((video) => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title={video.name}
+        />
+      ))}
+    </div>
+  );
 }
